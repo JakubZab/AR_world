8c938bc86d9bcbd566fd3cd56f627877540720c4
JakubZab <jakub18zaba@gmail.com>
2024-09-03 20:57:13 +0200

    First commit

--- Assets/Scripts/CharacterSelectionManager.cs
+++ Assets/Scripts/CharacterSelectionManager.cs
@@ -5,11 +5,9 @@ public class CharacterSelectionManager : MonoBehaviour
 {
     public void SelectCharacter(string characterName)
     {
-        // Przechowaj wybrany charakter, np. w PlayerPrefs
         PlayerPrefs.SetString("SelectedCharacter", characterName);
         PlayerPrefs.Save();
 
-        // Za³aduj scenê AR
         SceneManager.LoadScene("GameMode");
     }
 }
---
+++ README.md
@@ -0,0 +1,2 @@
+# HunterAR_NavMesh
+RPG game using NavMesh to navigate character.
